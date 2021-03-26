7d7f49a526f383d6bb00276138e1f4174efe6f96
Shandilyathithyaa <73104941+Shandilyathithyaa@users.noreply.github.com>
2021-03-26 19:03:26 +0530

    Create spamhi.js

---
+++ task-2/spamhi.js
@@ -0,0 +1,7 @@
+function spamhi()
+{
+    let textbox=document.querySelector(".composer_rich_textarea");
+    let sendbtn=document.querySelector(".btn btn-md im_submit im_submit_send");
+    textbox.innerHTML="Hi";
+    sendbtn.click();
+}

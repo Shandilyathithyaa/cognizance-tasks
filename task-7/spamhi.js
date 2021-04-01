f760b47701768102a5d1d1c3f374e1cd4e7af374
Shandilyathithyaa <73104941+Shandilyathithyaa@users.noreply.github.com>
2021-04-01 13:37:50 +0530

    Update spamhi.js

--- task-7/spamhi.js
+++ task-7/spamhi.js
@@ -1,7 +1,7 @@
 function spamhi()
 {
     let textbox=document.querySelector(".composer_rich_textarea");
-    let sendbtn=document.querySelector(".btn btn-md im_submit im_submit_send");
+    let sendbtn=document.querySelector("body > div.page_wrap > div.im_page_wrap.clearfix > div > div.im_history_col_wrap.noselect.im_history_loaded > div.im_history_selected_wrap > div > div.im_bottom_panel_wrap > div.im_send_panel_wrap.noselect > div > div > div > form > div.im_send_buttons_wrap.clearfix > button > span.im_submit_send_label.nocopy");
     textbox.innerHTML="Hi";
     sendbtn.click();
 }

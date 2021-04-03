37075de9f72fa0ed97a1baa8905d595a885db584
Shandilyathithyaa <73104941+Shandilyathithyaa@users.noreply.github.com>
2021-04-03 18:32:23 +0530

    Update spamhi.js

--- task-7/spamhi.js
+++ task-7/spamhi.js
@@ -1,9 +1,10 @@
 function spamhi()
 {
     let textbox=document.querySelector(".composer_rich_textarea");
-    let sendbtn=document.querySelector("body > div.page_wrap > div.im_page_wrap.clearfix > div > div.im_history_col_wrap.noselect.im_history_loaded > div.im_history_selected_wrap > div > div.im_bottom_panel_wrap > div.im_send_panel_wrap.noselect > div > div > div > form > div.im_send_buttons_wrap.clearfix > button > span.im_submit_send_label.nocopy");
+    //let sendbtn=document.querySelector("body > div.page_wrap > div.im_page_wrap.clearfix > div > div.im_history_col_wrap.noselect.im_history_loaded > div.im_history_selected_wrap > div > div.im_bottom_panel_wrap > div.im_send_panel_wrap.noselect > div > div > div > form > div.im_send_buttons_wrap.clearfix > button > span.im_submit_send_label.nocopy");
     textbox.innerHTML="Hi";
-    sendbtn.click();
+    //sendbtn.click();
+    $('.im_submit').trigger('mousedown');
 }
 
 setInterval(spamhi,2000);
